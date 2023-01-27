84c8a89209a41f16180a58ca343f3e161e95cb51
Wannasak Nooplod <6510110421@psu.ac.th>
2023-01-27 18:09:50 +0700

    แก้ไขinsertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,2 +1,14 @@
-wq
-[A[Cw:wq
+def insertion_sort(a):
+	if (n := len(a)) <= 1:
+	return
+	for i in range(1, n):		
+		key = a[i]
+		j = i-1
+		while j >=0 and key < a[j] :
+				a[j+1] = a[j]
+				j -= 1
+		a[j+1] = key
+
+a = [12, 11, 13, 5, 6]
+insertion_sort(a)
+print(a)
